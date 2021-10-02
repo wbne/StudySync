7650e76c4fda8816aab4131ee7d48a58aeb9330c
wbne <firetruckbw@gmail.com>
2021-10-02 01:32:57 -0500

    Create basic onboarding

---
+++ src/Onboarding.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './slices/counterSlice'
+import Button from '@material-ui/core/Button';
+
+/*
+ * Component that allows users to either sync their google account or continue as a guest.
+ * If they use a google account then check if there is data, if there is not, continue the form as if they were a guest.
+ * 	If there is data then go straight to the Jitsi page
+ *
+ * returns: two buttons
+ */
+const Onboarding = () => {
+	const dispatch = useDispatch()
+	return(
+		<>
+			<Button variant="outlined">Log in with Google</Button>
+			<Button 
+				variant="outlined"
+				onClick={() => dispatch(increment())}
+			>
+			Continue as a Guest</Button>
+		</>
+	);
+
+}
+
+export default Onboarding
---
+++ src/Subjects.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { setSubjects, addSubject } from './slices/subjectSlice'
+import { increment, decrement } from './slices/counterSlice'
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+
+//Hardcoded subjects which will make presentation and demonstration easier and cleaner
+const SUBJECTS = ["math", "sci", "hist"]
+
+/*
+ * Presents a list of subjects and asks the user to select the topics they would like to study for.
+ * The selected data is stored as an object in the redux storage in the following format:
+ * 	[SUBJECT_NAME]: [CHECKED]
+ * 	String        :  boolean
+ *
+ * Returns a list of checkboxes and some text
+ */
+export default function Subjects() {
+  const checked = useSelector((state) => state.subject.value)
+  const dispatch = useDispatch()
+  const handleChange = (event) => {
+    dispatch(addSubject(event.target.value))
+  };
+
+  /*
+   * Returns JSX Checkbox objects from a string array
+   */
+  const subjBoxes = () => {
+	var list = []
+	SUBJECTS.forEach((value) => list.push(<FormControlLabel 
+		control={
+			<Checkbox
+				checked={checked.value}
+				value={value}
+				onChange={handleChange}
+				name={value}
+				key={value}
+			/>
+		} 
+		label={value}/>));
+	return list
+  };
+
+  return (
+    <>
+      <p>Please select the study subjects</p>
+      <FormGroup column="true">
+  	      {subjBoxes()}
+      </FormGroup>
+      <Button 
+	      variant="outlined"
+	      onClick={() => dispatch(decrement())}
+      >
+	      Back
+      </Button>
+      <Button 
+	      variant="outlined"
+	      onClick={() => dispatch(increment())}
+      >
+	      Next
+      </Button>
+      
+    </>
+  );
+}
+
